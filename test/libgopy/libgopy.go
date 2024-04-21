9c843b165fb7816334ce394f4d056f8aec1a9104
tde-nico <101901057+tde-nico@users.noreply.github.com>
2024-04-21 12:27:55 +0200

    module system beta

---
+++ test/libgopy/libgopy.go
@@ -0,0 +1,153 @@
+package libgopy
+
+/*
+#cgo LDFLAGS: ./libgopy/libgopy.so
+#include <stdlib.h>
+#include <stdarg.h>
+#include "../../lib/includes/libgopy.h"
+*/
+import "C"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func init() {
+	C.init()
+}
+
+func Finalize() {
+	C.finalize()
+}
+
+func Load(module string) error {
+	cstr := C.CString(module)
+	defer C.free(unsafe.Pointer(cstr))
+	res := C.load(cstr)
+	if res != 0 {
+		return fmt.Errorf("failed to load module: %v", module)
+	}
+	return nil
+}
+
+type Args struct {
+	list  *C.t_pyargs
+	count int
+}
+
+func (a *Args) init_args(args []any) {
+	var tmp *C.t_pyargs
+	for _, arg := range args {
+		tmp = (*C.t_pyargs)(C.malloc(C.sizeof_t_pyargs))
+
+		t := reflect.TypeOf(arg).String()
+		switch t {
+		case "float64":
+			double := C.double(arg.(float64))
+			tmp.value = unsafe.Pointer(&double)
+			tmp.t = 'f'
+		case "int64":
+			integer := C.int(arg.(int64))
+			tmp.value = unsafe.Pointer(&integer)
+			tmp.t = 'd'
+		case "int":
+			integer := C.int(arg.(int))
+			tmp.value = unsafe.Pointer(&integer)
+			tmp.t = 'd'
+		case "[]uint8":
+			cstr := C.CString(string(arg.([]byte)))
+			tmp.value = unsafe.Pointer(cstr)
+			tmp.t = 'b'
+		case "string":
+			cstr := C.CString(arg.(string))
+			tmp.value = unsafe.Pointer(cstr)
+			tmp.t = 'b'
+		default:
+			fmt.Printf("Unknown type: %v\n", t)
+			continue
+		}
+		tmp.next = nil
+
+		a.count += 1
+		if a.list == nil {
+			a.list = tmp
+		} else {
+			tmp.next = a.list
+			a.list = tmp
+		}
+	}
+}
+
+func (a *Args) free() {
+	var tmp *C.t_pyargs
+
+	if a.list == nil {
+		return
+	}
+	for arg := a.list; arg != nil; {
+		tmp = arg.next
+		switch arg.t {
+		case 'b':
+			C.free(arg.value)
+		default:
+		}
+		C.free(unsafe.Pointer(arg))
+		arg = tmp
+	}
+}
+
+func Call_f64(fun string, args ...any) float64 {
+	str := fun
+	cstr := C.CString(str)
+	defer C.free(unsafe.Pointer(cstr))
+
+	var a Args
+	a.init_args(args)
+	defer a.free()
+
+	res := C.call_f64(cstr, C.int(a.count), a.list)
+
+	return float64(res)
+}
+
+func Call_i64(fun string, args ...any) int64 {
+	str := fun
+	cstr := C.CString(str)
+	defer C.free(unsafe.Pointer(cstr))
+
+	var a Args
+	a.init_args(args)
+	defer a.free()
+
+	res := C.call_i64(cstr, C.int(a.count), a.list)
+
+	return int64(res)
+}
+
+func Call_byte(fun string, args ...any) []byte {
+	str := fun
+	cstr := C.CString(str)
+	defer C.free(unsafe.Pointer(cstr))
+
+	var a Args
+	a.init_args(args)
+	defer a.free()
+
+	r := C.call_byte(cstr, C.int(a.count), a.list)
+	res := C.GoBytes(unsafe.Pointer(r.bytes), C.int(r.size))
+
+	return res
+}
+
+func Call(fun string, args ...any) {
+	str := fun
+	cstr := C.CString(str)
+	defer C.free(unsafe.Pointer(cstr))
+
+	var a Args
+	a.init_args(args)
+	defer a.free()
+
+	C.call(cstr, C.int(a.count), a.list)
+}
--- test/main.go
+++ test/main.go
@@ -1,162 +1,27 @@
 package main
 
-/*
-#cgo LDFLAGS: ./libgopy.so
-#include <stdlib.h>
-#include <stdarg.h>
-#include "../lib/includes/libgopy.h"
-*/
-import "C"
 import (
 	"fmt"
-	"reflect"
-	"unsafe"
+	"test/libgopy"
 )
 
-type Args struct {
-	list  *C.t_pyargs
-	count int
-}
-
-func (a *Args) init_args(args []any) {
-	var tmp *C.t_pyargs
-	for _, arg := range args {
-		tmp = (*C.t_pyargs)(C.malloc(C.sizeof_t_pyargs))
-
-		t := reflect.TypeOf(arg).String()
-		switch t {
-		case "float64":
-			double := C.double(arg.(float64))
-			tmp.value = unsafe.Pointer(&double)
-			tmp.t = 'f'
-		case "int64":
-			integer := C.int(arg.(int64))
-			tmp.value = unsafe.Pointer(&integer)
-			tmp.t = 'd'
-		case "int":
-			integer := C.int(arg.(int))
-			tmp.value = unsafe.Pointer(&integer)
-			tmp.t = 'd'
-		case "[]uint8":
-			cstr := C.CString(string(arg.([]byte)))
-			tmp.value = unsafe.Pointer(cstr)
-			tmp.t = 'b'
-		case "string":
-			cstr := C.CString(arg.(string))
-			tmp.value = unsafe.Pointer(cstr)
-			tmp.t = 'b'
-		default:
-			fmt.Printf("Unknown type: %v\n", t)
-			continue
-		}
-		tmp.next = nil
-
-		a.count += 1
-		if a.list == nil {
-			a.list = tmp
-		} else {
-			tmp.next = a.list
-			a.list = tmp
-		}
-	}
-}
-
-func (a *Args) free() {
-	var tmp *C.t_pyargs
-
-	if a.list == nil {
-		return
-	}
-	for arg := a.list; arg != nil; {
-		tmp = arg.next
-		switch arg.t {
-		case 'b':
-			C.free(arg.value)
-		default:
-		}
-		C.free(unsafe.Pointer(arg))
-		arg = tmp
-	}
-}
-
-func call_f64(fun string, args ...any) {
-	str := fun
-	cstr := C.CString(str)
-	defer C.free(unsafe.Pointer(cstr))
-
-	var a Args
-	a.init_args(args)
-	defer a.free()
-
-	res := C.call_f64(cstr, C.int(a.count), a.list)
-
-	fmt.Printf("%v: %v\n", fun, res)
-}
-
-func call_i64(fun string, args ...any) {
-	str := fun
-	cstr := C.CString(str)
-	defer C.free(unsafe.Pointer(cstr))
-
-	var a Args
-	a.init_args(args)
-	defer a.free()
-
-	res := C.call_i64(cstr, C.int(a.count), a.list)
-
-	fmt.Printf("%v: %v\n", fun, res)
-}
-
-func call_byte(fun string, args ...any) {
-	str := fun
-	cstr := C.CString(str)
-	defer C.free(unsafe.Pointer(cstr))
-
-	var a Args
-	a.init_args(args)
-	defer a.free()
-
-	r := C.call_byte(cstr, C.int(a.count), a.list)
-	res := C.GoBytes(unsafe.Pointer(r.bytes), C.int(r.size))
-
-	fmt.Printf("%v: %v -> %v\n", fun, res, string(res))
-}
-
-func call(fun string, args ...any) {
-	str := fun
-	cstr := C.CString(str)
-	defer C.free(unsafe.Pointer(cstr))
-
-	var a Args
-	a.init_args(args)
-	defer a.free()
-
-	C.call(cstr, C.int(a.count), a.list)
-}
-
 func main() {
-	C.init()
+	libgopy.Load("test_script")
 
-	str := "test_script"
-	cstr := C.CString(str)
-	defer C.free(unsafe.Pointer(cstr))
-	res1 := C.load(cstr)
-	fmt.Printf("res1: %v\n", res1)
-	if res1 != 0 {
-		return
-	}
+	resf := libgopy.Call_f64("func6")
+	fmt.Printf("resf: %v\n", resf)
+	resi := libgopy.Call_i64("func1")
+	fmt.Printf("resi: %v\n", resi)
+	resb := libgopy.Call_byte("func5")
+	fmt.Printf("resb: %v -> %v\n", resb, string(resb))
 
-	call_f64("func6")
-	call_i64("func1")
-	call_byte("func5")
+	libgopy.Call_f64("func7", 6.5, 10.0, 9.7, 8.2)
+	libgopy.Call_i64("func7", 6, 10, 9, 8)
+	libgopy.Call_byte("func7", "Hello", "World", "Go", "Python")
+	libgopy.Call_byte("func7", []byte("Hello"), []byte("World"), []byte("Go"), []byte("Python"))
+	libgopy.Call_f64("func7", 6.5, 10, "Hello", []byte("World"), int64(3))
 
-	call_f64("func7", 6.5, 10.0, 9.7, 8.2)
-	call_i64("func7", 6, 10, 9, 8)
-	call_byte("func7", "Hello", "World", "Go", "Python")
-	call_byte("func7", []byte("Hello"), []byte("World"), []byte("Go"), []byte("Python"))
-	call_f64("func7", 6.5, 10, "Hello", []byte("World"), int64(3))
+	libgopy.Call("func7", 6.5, 10, "Hello", []byte("World"), int64(3))
 
-	call("func7", 6.5, 10, "Hello", []byte("World"), int64(3))
-
-	C.finalize()
+	libgopy.Finalize()
 }
